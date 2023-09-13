edcdc16bbd6a3344ab5d1245498f055566509c90
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-09-13 10:33:48 -0700

    daily challenge day 3

---
+++ Week2/Day3/Daily challange day3/script.js
@@ -0,0 +1,15 @@
+
+
+// Ex 
+// 1. Write a JavaScript program that recreates the pattern below.
+// 2. Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// 3. Do this Daily Challenge by yourself, without looking at the answers on the internet.
+
+let star = '*';
+let space = ' ';
+
+for (let i = 0; i < 6; i++){
+  space = space + ' * ';
+  console.log(space);
+ 
+}
