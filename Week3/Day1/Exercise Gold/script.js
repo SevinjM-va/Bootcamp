abfd02a2e815eb2d762bd43df60f4543150c6939
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-09-18 14:53:17 -0700

    EX Gold

---
+++ Week3/Day1/Exercise Gold/script.js
@@ -0,0 +1,27 @@
+
+
+let cellChoice1 = document.querySelectorAll('tr')[0].children[0].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[0].children[2].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[0].children[4].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[1].children[1].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[1].children[3].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[2].children[0].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[2].children[2].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[2].children[4].style.backgroundColor = 'red'
+
+cellChoice1 = document.querySelectorAll('tr')[3].children[1].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[3].children[3].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[4].children[0].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[4].children[2].style.backgroundColor = 'red';
+
+cellChoice1 = document.querySelectorAll('tr')[4].children[4].style.backgroundColor = 'red'
