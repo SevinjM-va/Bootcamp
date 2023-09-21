d17afa4fb5e787761c3c2c14c771fd5faffbfcc4
nergız seyidova <nergisseyidova@gmail.com>
2023-09-21 11:30:47 +0400

    checker comments

--- Week3/Day3/Daily challenge/script.js
+++ Week3/Day3/Daily challenge/script.js
@@ -1,13 +1,24 @@
-
-
 let input = document.getElementById('onlyText');
+let container = document.getElementById('container');
 
+// function choiceLetter(event){
+//   console.log(event);
+// //   if (
+// //     String.fromCharCode(event.keyCode) !== '%' &&
+// //     (event.keyCode == 8 || event.keyCode == 37 || event.keyCode == 39 || englishAlphabetAndWhiteSpace.test(key))
+// //   ){
+// //     console.log(input);
+// // }
+// }
 
-function choiceLetter(event){
-  if (
-    String.fromCharCode(event.keyCode) !== '%' && 
-    (event.keyCode == 8 || event.keyCode == 37 || event.keyCode == 39 || englishAlphabetAndWhiteSpace.test(key))
-  ){
-    console.log(input);
-}
-}
+input.addEventListener('keydown', (e) => {
+  console.log(e.key);
+  const inputKey = e.key;
+  const regex = /[a-zA-Z]/;
+
+  if (regex.test(inputKey)) {
+    const p = document.createElement('p');
+    p.textContent = inputKey;
+    container.appendChild(p);
+  }
+});
