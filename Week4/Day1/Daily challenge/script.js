d92f895d3647d5eacacea2b5aa5b863e01b85bfb
nergız seyidova <nergisseyidova@gmail.com>
2023-09-26 11:15:57 +0400

    checker comments

--- Week4/Day1/Daily challenge/script.js
+++ Week4/Day1/Daily challenge/script.js
@@ -1,40 +1,48 @@
 const gameInfo = [
   {
-    username: "john",
-    team: "red",
+    username: 'john',
+    team: 'red',
     score: 5,
-    items: ["ball", "book", "pen"]
+    items: ['ball', 'book', 'pen'],
   },
   {
-    username: "becky",
-    team: "blue",
+    username: 'becky',
+    team: 'blue',
     score: 10,
-    items: ["tape", "backpack", "pen"]
+    items: ['tape', 'backpack', 'pen'],
   },
   {
-    username: "susy",
-    team: "red",
+    username: 'susy',
+    team: 'red',
     score: 55,
-    items: ["ball", "eraser", "pen"]
+    items: ['ball', 'eraser', 'pen'],
   },
   {
-    username: "tyson",
-    team: "green",
+    username: 'tyson',
+    team: 'green',
     score: 1,
-    items: ["book", "pen"]
+    items: ['book', 'pen'],
   },
- ];
+];
 
 //  !!!Burada usernames-ləri push edirəm arraya alınmır
-// 
-// 
+//
+//
 
- let arr = [];
- let username;
-gameInfo.forEach((el)=>{
-  username = [el.username+'!'];
+let arr = [];
+let username;
+let winners = [];
+gameInfo.forEach((el) => {
+  username = el.username + '!'; /* array syntaxina ehtiyac yox idi */
   arr.push(username);
-  // console.log(username);
-  let winners = el.score;
-  console.log(winners);
-})
+  console.log(
+    arr
+  ); /* yeqinki burda arr yerine username i console etdiyin ucun qarisib netice duzgun idi */
+
+  // !!! task 2
+  if (el.score > 5) {
+    winners.push(el.username);
+  }
+  
+});
+console.log(winners);
