596d508ca3f7a933dd36a99f8636bfcb47940009
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-10-02 12:36:11 -0700

    DC

--- Week5/Day1/Daily Challenge/script.js
+++ Week5/Day1/Daily Challenge/script.js
@@ -2,15 +2,28 @@
 
 let first = document.getElementById('name');
 let second = document.getElementById('surname');
-const submitbtn = document.querySelector('submitbtn');
+const form = document.querySelector('form');
 
 
-submitbtn.addEventListener('submit', submit);
 
-function submit(){
-  console.log('hello')
+
+form.addEventListener('submit', submit);
+
+function submit(event){
+  event.preventDefault();
+  const name = form.name.name ;
+  const nameValue = form.name.value;
+  let joinname = JSON.stringify(name) +":"+ JSON.stringify(nameValue);
+  
+  const surname = form.surname.name ;
+  const surnameValue = form.surname.value;
+  let joinSurname = JSON.stringify(surname) +":"+ JSON.stringify(surnameValue);
+
+  let join = JSON.stringify(joinname+joinSurname);
+
+  let p = document.createElement('p');
+  p.innerText = join;
+  document.body.appendChild(p);
 }
 
-const btn = ()=>{
 
-}
