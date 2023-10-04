850cc7aa0f8a3e9e78dfec16fca35182ca225847
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-10-04 11:36:29 -0700

    Gold

---
+++ Week5/Day3/ExGold/script.js
@@ -0,0 +1,126 @@
+const urls = [
+  'https://www.swapi.tech/api/people/1',
+  'https://www.swapi.tech/api/people/2',
+  'https://www.swapi.tech/api/people/3',
+  'https://www.swapi.tech/api/people/4'
+]
+console.log(urls);
+
+
+// !!! Resolve və rejecti onload funsiyasının icərisinə saldıqda işləmir.
+// !!! Burada hər promise üçün ayrıca neü promise yaradılmalıdir yoxsa loopla etsək kifayətdirmi? allPromiseə aid tapşırıqlarda ayrı ayrı yaradılmışdı deyə mən də elə etdim amma məncə qısa variantı da var. Zəhmət olmasa deyərsiniz necə etmək lazım olduğunu.
+
+const prom1 = new Promise ((resolve, reject)=>{
+// for(let i = 0; i < urls.length; i++){
+let x = new XMLHttpRequest();
+x.open('GET', urls[0]);
+
+x.responseType = 'json';
+x.send();
+
+x.onload = function(){
+if(x.status != 200){
+  console.log('something went wrong')
+} else {
+
+// !!! Burada name-i properties obyektinə daxil edə bilmədim.
+  let first = x.response.result.properties.name;
+  let second = x.response.result.properties;
+  console.log(first);
+  console.log(second);
+}
+}
+x.onerror = function(){
+  alert('Request failed');
+};
+// }
+}).then(result=>console.log(result));
+
+
+
+
+const prom2 = new Promise ((resolve, reject)=>{
+  // for(let i = 0; i < urls.length; i++){
+  let x = new XMLHttpRequest();
+  x.open('GET', urls[1]);
+  
+  x.responseType = 'json';
+  x.send();
+  
+  x.onload = function(){
+  if(x.status != 200){
+    console.log('something went wrong')
+  } else {
+    let first = x.response.result.properties;
+    console.log(first) 
+  }
+  }
+  x.onerror = function(){
+    alert('Request failed');
+  };
+  // }
+  }).then(result=>console.log(result));
+
+  
+
+  const prom3 = new Promise ((resolve, reject)=>{
+    // for(let i = 0; i < urls.length; i++){
+    let x = new XMLHttpRequest();
+    x.open('GET', urls[2]);
+    
+    x.responseType = 'json';
+    x.send();
+    
+    x.onload = function(){
+    if(x.status != 200){
+      console.log('something went wrong')
+    } else {
+      let first = x.response.result.properties;
+      console.log(first) 
+    }
+    }
+    x.onerror = function(){
+      alert('Request failed');
+    };
+    // }
+    }).then(result=>console.log(result));
+
+    
+
+
+    const prom4 = new Promise ((resolve, reject)=>{
+      // for(let i = 0; i < urls.length; i++){
+      let x = new XMLHttpRequest();
+      x.open('GET', urls[3]);
+      
+      x.responseType = 'json';
+      x.send();
+      
+      x.onload = function(){
+      if(x.status != 200){
+        console.log('something went wrong')
+      } else {
+        let first = x.response.result.properties;
+        console.log(first) 
+      }
+      }
+      x.onerror = function(){
+        alert('Request failed');
+      };
+      // }
+      }).then(result=>console.log(result));
+
+      
+
+
+Promise.all([prom1, prom2, prom3, prom4])
+.then(result => console.log(result))
+.catch(error => console.log(error));
+
+Promise.allSettled([prom1, prom2, prom3, prom4])
+.then(result => console.log(result))
+.catch(error => console.log(error));
+
+Promise.race([prom1, prom2, prom3, prom4])
+.then(result => console.log(result))
+.catch(error => console.log(error));
