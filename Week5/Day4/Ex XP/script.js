851242865499a315daaee0f6b1e251d5663ee530
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-10-05 04:18:19 -0700

    ex XP

---
+++ Week5/Day4/Ex XP/script.js
@@ -0,0 +1,31 @@
+
+// Ex 1
+// fetch("https://www.swapi.tech/api/starships/9/")
+//     (response => response.json())
+//     .then(objectStarWars => console.log(objectStarWars.result));
+
+
+
+async function a (){
+  const result = await (await fetch("https://www.swapi.tech/api/starships/9/")).json();
+  console.log(result);
+};
+a();
+
+
+// Ex 2
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+      setTimeout(() => {
+          resolve('resolved');
+      }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  let result = await resolveAfter2Seconds();
+  console.log(result);
+}
+
+asyncCall();
