ac5a3d742ae9cd0edd11a34b290b47d82b495961
nergız seyidova <nergisseyidova@gmail.com>
2023-10-19 17:00:53 +0400

    checker comments

--- Week7/Day2/ExXP/blog-api/server.js
+++ Week7/Day2/ExXP/blog-api/server.js
@@ -1,66 +1,66 @@
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(express.json());
 
-
-app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const array = [
   {
-  id: 2525,
-  title: 'hi',
-  content: 'djwkklwjo ojdoqwjdqowj oqjweoqwejqowjeq jqowejqow'
-},
-{
-  id: 7898,
-  title: 'good-bye',
-  content: 'djwkklwjo ojdoqwjdqowj oqjweoqwejqowjeq jqowejqow'
-},
-{
-  id: 6666,
-  title: 'hello',
-  content: 'djwkklwjo ojdoqwjdqowj oqjweoqwejqowjeq jqowejqow'
-}
+    id: 2525,
+    title: 'hi',
+    content: 'djwkklwjo ojdoqwjdqowj oqjweoqwejqowjeq jqowejqow',
+  },
+  {
+    id: 7898,
+    title: 'good-bye',
+    content: 'djwkklwjo ojdoqwjdqowj oqjweoqwejqowjeq jqowejqow',
+  },
+  {
+    id: 6666,
+    title: 'hello',
+    content: 'djwkklwjo ojdoqwjdqowj oqjweoqwejqowjeq jqowejqow',
+  },
 ];
 
-app.get('/posts', (req,res)=>{
-  res.json(array)
+app.get('/posts', (req, res) => {
+  res.json(array);
 });
 
-
-app.get('/posts/:id', (req,res)=>{
+app.get('/posts/:id', (req, res) => {
   const id = Number(req.params.id);
-  const comp = array.find(el => el.id === id);
-  res.json(comp)
+  const comp = array.find(
+    (el) => el.id === id
+  ); /* postman type da problem olarsa == */
+
+  res.json(comp);
 });
 
-
-app.post('/posts',(req,res)=>{
- const body = req.body;
- array.push(body);
- res.json(body);
+app.post('/posts', (req, res) => {
+  const body = req.body;
+  //  !!! req body nin olub olmamasini if statementle yoxla
+  array.push(body);
+  res.json(body);
 });
 
-
-app.put('/posts/:id',(req,res)=>{
+app.put('/posts/:id', (req, res) => {
   const id = Number(req.params.id);
   const body = req.body;
-  const index = array.findIndex((item)=>{
+  // !!! object id meselesini nezere al yeni bir object yaradib hazir id ni array length mentiqi ile isleye bilersen
+  //  !!! req body nin olub olmamasini if statementle yoxla
+  const index = array.findIndex((item) => {
     return item.id == id;
-  })
+  });
   array[index] = body;
   res.status(200).json(body);
 });
 
-
-
-app.delete('/posts/:id',(req,res)=>{
+app.delete('/posts/:id', (req, res) => {
   const findId = posts.params.id;
-  const comp = array.findIndex(item => item.id === findId)
-  res.json(comp)
+  const comp = array.findIndex(
+    (item) => item.id === findId
+  ); /* postman type da problem olarsa == */
+  // !!! silmek ucun splice metodundan istifade et
+  res.json(comp);
 }); //Burada error verir. Niye bilmirem.
 app.listen(3000);
-
--- Week7/Day2/ExXP/book-api/app.js
+++ Week7/Day2/ExXP/book-api/app.js
@@ -1,52 +1,51 @@
-
 const express = require('express');
 const app = express();
-
 
 const books = [
   {
     id: 1,
     title: 'Harry Potter',
     author: 'J.K.Rowling',
-    publishedYear: 1999
+    publishedYear: 1999,
   },
   {
     id: 2,
     title: 'Murder',
     author: 'M.K. Anderson',
-    publishedYear: 2010
+    publishedYear: 2010,
   },
   {
     id: 3,
     title: 'Alice',
     author: 'K.K.John',
-    publishedYear: 2022
+    publishedYear: 2022,
   },
   {
     id: 4,
     title: 'My Mom',
     author: 'L.K.Sarah',
-    publishedYear: 1990
-  }
-]
+    publishedYear: 1990,
+  },
+];
 
-
-app.get('/api/books',(req,res)=>{
-  res.json(books)
-})
-get.post('/api/books',(req,res)=>{
+app.get('/api/books', (req, res) => {
+  res.json(books);
+});
+get.post('/api/books', (req, res) => {
   const body = req.body;
   books.push(body);
   res.status(201).send('work');
 });
 
-app.get('/api/books/:bookId',(req,res)=>{
+app.get('/api/books/:bookId', (req, res) => {
+  // !!
+  /* eger pathde dynamic sekilde :bookId yazirsansa eyni adla istifade etmelisen */
   const id = parseInt(req.params.id);
-  const comp = books.find(el=>el.id === id);
-  res.send(comp)
+  const comp = books.find(
+    (el) =>
+      el.id === id /* bookId olmalidi elave olaraq das type lara fikir ver*/
+  );
+  res.send(comp);
 }); //Bu kodda error verir ve daxil edilmis parametri NaN kimi verir;
-
-
-
 
 app.listen(5000);
