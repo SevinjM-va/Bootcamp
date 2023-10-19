9311a613387890333d59b71eb9a95930b1cc9479
nergız seyidova <nergisseyidova@gmail.com>
2023-10-19 11:46:21 +0400

    checker comments

--- Week7/Day2/ExXP/crud-api/app.js
+++ Week7/Day2/ExXP/crud-api/app.js
@@ -8,4 +8,17 @@ app.get('/',(req,res)=>{
   res.json(fetchPosts)
 })
 
+// !!! solution
+app.get('/', async (req, res) => {
+  try {
+    const posts = await fetchPosts.fetchPosts(); /* file icerisinden bize lazim olani gotururuk */
+    
+    res.json(posts);
+    console.log('Data retrieved and sent as response.');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal server error');
+  }
+});
+// !!!
 app.listen(5000);
--- Week7/Day2/ExXP/crud-api/dataService.js
+++ Week7/Day2/ExXP/crud-api/dataService.js
@@ -8,4 +8,17 @@ function fetchPosts(){
 }
 //Buda status 200-du anca ekranda bir sey yoxdur
 
+// !!! solution
+async function fetchPosts() {/*  async olmali ki await islesin  */
+  try {/*  error cixarsa ne geldiyini gormek ucun try catch */
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    return response.data;
+  } catch (error) {
+    throw error; /* error versin deye */
+  }
+}
+// !!!
+
   module.exports = fetchPosts;
