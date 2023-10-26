97d8a2800c30d15f9e909d5e22ad1034e3dd2cb5
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-10-26 05:43:23 -0700

    exxp2

---
+++ Week8/Day4/Exxp2/blog/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostList from './Component/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      <PostList/>
+    </div>
+  );
+}
+
+export default App;
