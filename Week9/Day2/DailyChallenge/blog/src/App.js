ef72b63e90626a4bd829ce08386b17c9663b5bd0
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-11-02 12:34:26 -0700

    dc

---
+++ Week9/Day2/DailyChallenge/blog/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import  Robofriends from './Component/Robofriends';
+
+
+function App() {
+
+  return (
+    <div>
+      <Robofriends/>
+    </div>
+  );
+}  
+
+export default App;
---
+++ Week9/Day2/DailyChallenge/blog/src/Reducer.js
@@ -0,0 +1,12 @@
+import {createStore} from 'redux';
+
+
+const state = []
+// const maiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+function robotDetails (state,action){
+  // console.log(action.type)
+  return state
+}
+
+export const store = createStore(robotDetails);
